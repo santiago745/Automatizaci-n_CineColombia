aab7d3bba0afd2699cf41c7bc552204f4a49447b
Your Name <Admin@PE-HO-ATH-193.PERIFERIAGROUP.MS>
2019-07-09 10:29:10 -0500

    cambios2

---
+++ Test Suites/CC - UNION.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CC - UNION</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>50406855-1b83-4ad4-86e9-ce8256738831</testSuiteGuid>
+</TestSuiteEntity>
